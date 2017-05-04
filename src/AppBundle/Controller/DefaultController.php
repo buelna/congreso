52b897bf9debc2666448f05d336594c9447c6f25
Buelna <andres.buelna.b@gmail.com>
2017-05-04 12:01:41 -0700

    Contenido

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 class DefaultController extends Controller
 {
     /**
@@ -87,8 +84,7 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/venue.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,));
     }
     /**
      * @Route(
