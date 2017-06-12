a5ee2779dd9e27ff506a02a68821aa78a8a08a99
Buelna <andres.buelna.b@gmail.com>
2017-06-12 16:11:41 -0700

    content

--- src/AppBundle/Resources/public/assets/js/show-hide.js
+++ src/AppBundle/Resources/public/assets/js/show-hide.js
@@ -5,44 +5,13 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){ 
-   $('#MErequisitos').on('click',function(){
-      $('#MErequisitos1').toggle();
+   $('#carteles').on('click',function(){
+      $('#carteles1').toggle();
    });
 });
 
 $(document).ready(function(){ 
-   $('#MEapoyo').on('click',function(){
-      $('#MEapoyo1').toggle();
+   $('#coloquio').on('click',function(){
+      $('#coloquio1').toggle();
    });
 });
-
-$(document).ready(function(){ 
-   $('#IIplan').on('click',function(){
-      $('#IIplan1').toggle();
-   });
-});
-
-$(document).ready(function(){ 
-   $('#IIgeneral').on('click',function(){
-      $('#IIgenera1').toggle();
-   });
-});
-
-$(document).ready(function(){ 
-   $('#IIacademica').on('click',function(){
-      $('#IIacademica1').toggle();
-   });
-});
-
-$(document).ready(function(){ 
-   $('#MElineaA').on('click',function(){
-      $('#MElineaA1').toggle();
-   });
-});
-
-$(document).ready(function(){ 
-   $('#MElineaB').on('click',function(){
-      $('#MElineaB1').toggle();
-   });
-});
-
